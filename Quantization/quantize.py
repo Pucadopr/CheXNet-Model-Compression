f18578286dd1732af80b5b5bab5db511226c7435
Pelumi <pelumi@flutterwavego.com>
2020-10-29 03:50:18 +0100

    Add dynamic quantization

--- Quantization/quantize.py
+++ Quantization/quantize.py
@@ -1,9 +1,19 @@
 from torch.quantization import QuantStub, DeQuantStub
 import torch
+import torch.nn as nn
 
-def quantize(model, inplace=True):
+def static_quantize(model, inplace=True):
+
+    torch.backends.quantized.engine = 'fbgemm'
     model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
     torch.quantization.prepare(model, inplace)
     torch.quantization.convert(model.cpu(), inplace)
-
+    
     return model
+
+def dynamic_quantize(model):
+
+    torch.quantization.quantize_dynamic(model, {nn.Sequential}, dtype=torch.qint8)
+    return model
+
+    
