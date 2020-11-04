542a9d6c513a618b3a8a2a1d753ef2e87168d75b
Pelumi <pelumi@flutterwavego.com>
2020-11-04 19:15:46 +0100

    Add quantization aware training method

--- Quantization/quantize.py
+++ Quantization/quantize.py
@@ -1,19 +1,52 @@
+"""
+Methods for quantizing model.
+"""
 from torch.quantization import QuantStub, DeQuantStub
 import torch
 import torch.nn as nn
 
 def static_quantize(model, inplace=True):
-
+    '''
+    model passed must be set to eval mode (model.eval()) for static 
+    quantization logic to work
+    '''
     torch.backends.quantized.engine = 'fbgemm'
     model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
-    torch.quantization.prepare(model, inplace)
-    torch.quantization.convert(model.cpu(), inplace)
-    
-    return model
+    # model= torch.quantization.fuse_modules(model, [['conv', 'relu']])
+    model = torch.quantization.prepare(model, inplace)
+    model = torch.quantization.convert(model.cpu(), inplace)
+    return model     
 
 def dynamic_quantize(model):
-
-    torch.quantization.quantize_dynamic(model, {nn.Sequential}, dtype=torch.qint8)
+    '''
+    model passed must be set to an instance of model (model()) 
+    for dynamic quantize logic to work
+    '''
+    model = torch.quantization.quantize_dynamic(model, {nn.Sequential}, dtype=torch.qint8)
     return model
+
+def qat_quantize_prepare(model, inplace=True):
+    '''
+    model passed must be set to train mode (model.train()) for quantization 
+    aware training logic to work
+    '''
+    torch.backends.quantized.engine = 'fbgemm'
+    model.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm')
+    model = torch.quantization.prepare_qat(model, inplace)
+    return model
+
+def qat_quantize_eval(model):
+    '''
+    model passed must have been custom trained
+    '''
+    model.eval()
+    model = torch.quantization.convert(model)
+    return model
+
+
+
+
+
+
 
     
