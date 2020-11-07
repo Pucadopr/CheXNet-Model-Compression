7f77b2e4dba5a4e52a9c1e22c702eb61de40184b
Pelumi <pelumi@flutterwavego.com>
2020-11-07 22:52:48 +0100

    Add get data path method

--- data.py
+++ data.py
@@ -1,49 +1,39 @@
 """
-    Python script used for downloading all the datasets used in this
-    project. 
-    Data are downloaded to root folder
-
-    python data.py --dataset='RSNA'
+Python script used for downloading all the datasets used in this
+project. 
+Data are downloaded to root folder
 """
-
-import argparse
 import academictorrents as at
+from torch.utils import data
+import tarfile
+import os
 
 
-
-
-parser = argparse.ArgumentParser()
-
-# The dataset downloaded are the resized ones (224x224)
-parser.add_argument('--dataset', default='NIH', choices=['NIH', 'RSNA', 'Pad-Chest', 'Openi'],
-                    help='dataset name')
-
-# Add more parser as need requires
-args = parser.parse_args()
-
-# dictionary of hash to download data from academictorrents
 data_hash_dict = {'NIH': "e615d3aebce373f1dc8bd9d11064da55bdadede0",
                   'RSNA': "95588a735c9ae4d123f3ca408e56570409bcf2a9",
                   'Pad-Chest': "96ebb4f92b85929eadfb16761f310a6d04105797",
                   'Openi': "5a3a439df24931f410fac269b87b050203d9467d"}
 
 
-# Print the dataset that its downloading
-data_path = at.get(data_hash_dict.get(args.dataset)) # Download mnist dataset
-print(data_path)
+def get_nih_data_paths():
+    '''
+    returns the path to the nih images,
+    patient csv file and bbox csv file
+    '''
+    path = at.get(data_hash_dict.get('NIH'))
+    to_path = os.path.dirname(path)
+    path_to_tar= os.path.join(path, "images-224.tar")
+
+    with tarfile.open(path_to_tar, 'r') as tar:
+            tar.extractall(path=to_path)
+            tar.close()
+    
+    x_ray_data_path = os.path.join(path, "Data_Entry_2017.csv")
+    bbox_data_path = os.path.join(path, "BBox_List_2017.csv")
+    image_path = os.path.join(path, "images-224")
 
 
-# Uncomment if the downloaded is a tar file (.tar)
-# import tarfile
+    return image_path, x_ray_data_path, bbox_data_path
 
-# files = tarfile.open("/file.tar") # specify the path to the tar file
-# files.extractall("Data") # Specify the directory to extract too
-# files.close()
-
-# Uncomment if the downloaded is a zip file (.zip)
-# import zipfile
-
-# with zipfile.ZipFile("/path_file.zip", 'r') as zip_file:
-#     zip_file.extractall("Data")
 
 
