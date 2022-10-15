8f6617003f13c415e0160dd324643f4743a7e0e7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 16:30:01 +0000

    Adding tarfile member sanitization to extractall()

--- data.py
+++ data.py
@@ -27,7 +27,26 @@ def get_nih_data_paths():
     path_to_tar= os.path.join(path, "images-224.tar")
 
     with tarfile.open(path_to_tar, 'r') as tar:
-            tar.extractall(path=to_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=to_path)
             tar.close()
     
     x_ray_data_path = os.path.join(path, "Data_Entry_2017.csv")
